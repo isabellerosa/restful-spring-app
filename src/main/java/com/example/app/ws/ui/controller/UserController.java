f537b4cadff67ad00f5b316edec00dc7e7e306e3
Isabelle <ibell.pereira@gmail.com>
2019-08-09 15:19:45 -0300

    get users com paginacao

--- src/main/java/com/example/app/ws/ui/controller/UserController.java
+++ src/main/java/com/example/app/ws/ui/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.app.ws.ui.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.app.ws.exceptions.UserServiceException;
@@ -28,6 +32,22 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+
+	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public List<UserRest> getUsers(@RequestParam(value = "page", defaultValue = "1") int page,
+			@RequestParam(value = "limit", defaultValue = "25") int limit) {
+		List<UserRest> returnValue = new ArrayList<>();
+		
+		List<UserDto> users = userService.getUsers(page, limit);
+		
+		users.forEach(userDto -> {
+			UserRest userModel = new UserRest();
+			BeanUtils.copyProperties(userDto, userModel);
+			returnValue.add(userModel);
+		});
+
+		return returnValue;
+	}
 
 	@GetMapping(path = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
 	public UserRest getUser(@PathVariable String id) {
